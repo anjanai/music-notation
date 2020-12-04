d1d949c4802e2aecba6d560573e20d77beb0ccb5
Anjana Iyer <anjanai.yahoo@gmail.com>
2020-12-03 18:52:09 -0800

    Add playing notes

--- scales.js
+++ scales.js
@@ -1,5 +1,9 @@
 var scales = [];
 var derived_scales = [];
+var context = new AudioContext(),
+    oscillator;
+
+var notes = 'S r R g G m M P d D n N $'.split(' ' );
 
 function combinations(array) {
   return new Array(1 << array.length).fill().map(
@@ -36,17 +40,60 @@ function toScale(n) {
     return notes.join(' ');
 }
 
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function playOscillator(note) {
+    oscillator = context.createOscillator();
+    var  gain = context.createGain()
+    gain.connect(context.destination);
+
+    oscillator.connect(gain);
+
+    oscillator.frequency.value = getFreq(note);
+    oscillator.start(context.currentTime);
+    gain.gain.exponentialRampToValueAtTime(
+	0.00001, context.currentTime + 1.5
+    )
+
+}
+
+
+// S r R g G m M P d D n N
+
+const S = 262;
+function getFreq(note) {
+    n = notes.indexOf(note);
+    return S * Math.pow(2, n/12);
+}
+
+async function play(link) {
+    let scale = link.innerText.split(' ');
+    scale.push('$');
+    for (i of scale) {
+	playOscillator(i);
+	await sleep(800);
+    }
+    scale.reverse();
+    for (i of scale) {
+	playOscillator(i);
+	await sleep(800);
+    }
+}
+
 function createScales(n, ol) {
     if (n == 7)  {
 	for (let i=0; i<32; i++) {
-	    $(ol).append( '<li>' + toScale(i) + '</li>');
+	    $(ol).append( '<li><a href=# onClick=play(this)>' + toScale(i) + '</a></li>');
 	}
 	return;
     }
 
     for (let str of derived_scales.filter(a => a.length == n+n-1)) {
-	if (str.match(/g d/i)) str += " (No M/P)"
-	$(ol).append('<li>' + str + '</li>');
+	//if (str.match(/g d/i)) str += " (No M/P)"
+	$(ol).append('<li><a href=# onClick=play(this)>' + str + '</a></li>');
     }
 }
 
