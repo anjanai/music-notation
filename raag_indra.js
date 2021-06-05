28948ae8e146dda94ae86c40042cb03df28a2eab
Anjana Iyer <anjanai.yahoo@gmail.com>
2021-06-04 19:49:16 -0700

    Add tarana conversion

--- raag_indra.js
+++ raag_indra.js
@@ -1,4 +1,4 @@
-var scale = `X: raag_indra
+const scale = `X: raag_indra
 T: Raag Indra : scale
 L: 1/4
 R: Kavi Iyer
@@ -8,28 +8,65 @@ w: Sa ga ma Pa ni Sa | Sa ni Pa ma Re sa
 `;
 
 
-var tarana = `X: raag_indra_tarana
+const tarana_notation = `| - - - - | s r - s     | n, p, s n,  | p, n, p, m,  | 
+| - - - - | त नोम - त  | द  रे दा  नी   | ना दिर दिर त  |
+| p, - - -     | m p g g     | m r s n,   | s n, p, n,  |
+| नोम - - - | दे  रे ना दे    | रे ना  दा नी   | त न त न      |
+
+| s - - -     | p p m p     | n -   p p    | n p g m |
+| नोम - - - | य ली य ल    | लोम -  य ली   | य ल य ला |
+| p n p -    | n S p p      | n m g m    | p/p/ m/m/ p/p/ n/n/ |
+| ली य लोम -  | दे रे ना दे     | रे ना दा नी   | किड़ नग धुम किट |
+| S - - -   |
+| तोम - - - |
+`;
+
+let note = 'C';
+let notemap = new Map();
+for (let swar of "srgmpdn") {
+    notemap.set(swar, note);
+    notemap.set(swar.toUpperCase(), note.toLowerCase());
+    note = String.fromCharCode(note.charCodeAt() + 1)
+    if (note > 'G') note = 'A';
+}
+
+
+function convert_notation (str) {
+    str = str.replace(/ +/g, ' ');
+    let abc = `X: raag_indra_tarana
 T: Raag Indra : Tarana in drut teentaal
 L: 1/4
 M: 4/4
-Q:1/4=180
+Q:1/4=222
 R: Kavi Iyer
-K: Cdor % C doria(ga, ni komal)
-z4 | |: C D2 C | B, G, C B,  | G B G F | 
-w:        s r s | n p s n | p n p m | p
-w: त नोम त | द रे दा नी ना दिर दिर त | नोम
-% 
-G4 | :||: F G E E | F D C B, | C B, G, B, | 
-w: p      m p g g   m r s n    s n p n
-w: नोम    दे रे ना दे   रे ना दा नी   त न त न
-C4 | :|
-w: s
-w: नोम  
+K: Cdor % C dorian scale(ga, ni komal)
 `;
+    
+    re = /^[srgmpn/|,\-\s]+$/ig;
+    for (let line of str.split("\n")) {
+	if (! line.match(re)) {
+	    abc += "w:" + line.replaceAll("-", "") + "\n";
+	    continue;
+	}
+	notes = line.replaceAll("- - - -", "z4");
+	notes = notes.replaceAll(" - - -", "4");
+	notes = notes.replaceAll(" -", "2");
+	for (let i = 0; i < notes.length; i++)
+	    abc += ( notemap.get(notes[i]) || notes[i] );
+	abc += "\n";
+	line = line.replaceAll("-", "");
+	abc += "w:" + line + "\n";
+    }
+    console.log (abc);
+    return abc;
+}
+
+
+
 
 function loadNotation() {
     document.getElementById("abc-text1").value = scale;
-    document.getElementById("abc-text2").value = tarana;
+    document.getElementById("abc-text2").value = convert_notation(tarana_notation);
 }    
 
 
