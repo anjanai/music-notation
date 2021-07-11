b9bf23c3e21078699f8a64a05035dd4dfdcd1570
Anjana Iyer <anjanai.yahoo@gmail.com>
2021-07-10 21:45:35 -0700

    convert srgm to abcd

--- abc_play.js
+++ abc_play.js
@@ -1,13 +1,40 @@
+const srgm =
+      `,m ,M ,P ,d ,D ,n ,N
+s r R g    G m M P   d D n N
+s' r' R' g' G' m' M' P'`.split(/\s+/);
 
-var abc = "T: Cooley's\n" +
-    "M: 4/4\n" +
-    "L: 1/8\n" +
-    "R: reel\n" +
-    "K: Emin\n" +
+const abcd = `,F ,^F ,G ,^G ,A ,^A ,B
+C ^C D ^D    E F ^F G    ^G A ^A B
+c ^c d ^d    e f ^f g`.split(/\s+/);
+
+const notemap =  abcd.reduce(function(notemap, field, index) {
+  notemap[srgm[index]] = field;
+  return notemap;
+}, {})
+
+let key = "C#";  // C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+
+
+function convert_notation (notes) {
+    let abc = "";
+
+    for (let i = 0; i < notes.length; i++)
+	abc += ( notemap[notes[i]] || notes[i] );
+    console.log (abc);
+    return abc;
+}
+
+
+var abc =  
     "|:D2|EB{c}BA B2 EB|~B2 AB dBAG|FDAD BDAD|FDAD dAFD|\n" +
     "EBBA B2 EB|B2 AB defg|afe^c dBAF|DEFD E2:|\n" +
     "|:gf|eB B2 efge|eB B2 gedB|A2 FA DAFA|A2 FA defg|\n" +
     "eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|";
+
+abc = convert_notation (`Q: 1/4=100
+m, M, P, d, D, n, N,
+s r R g    G m M P   d D n N
+s' r' R' g' G' m' M' P'`);
 
 function load() {
     // NOTE: If you want just the sound without showing the music, use "*" instead of "paper" in the renderAbc call.
