bd4d18a03e6457e37bc71c1330125d9b652c4c1a
Anjana Iyer <anjanai.yahoo@gmail.com>
2022-03-13 10:43:38 -0700

    Add kafi.srg

--- compositions.js
+++ compositions.js
@@ -1,17 +1,79 @@
 let compositions = [
-    './harmonium/kafi.abc',
+    'harmonium/kafi',
 ];
 
-soMany = 22;
-console.log(`This is ${soMany} times easier!`);
+let note = "C"
+let notemap = new Map();
+for (let swar of "srgmpdn") {
+    notemap.set(swar, note);
+    notemap.set(swar.toUpperCase(), note.toLowerCase());
+    note = String.fromCharCode(note.charCodeAt() + 1)
+    if (note > 'G') note = 'A';
+}
+
+function convert_note(x) {
+    let str = notemap.get(x);
+    if (!str) return x;
+    return '"_' + x + '"' + str;
+}
+
 
 $(document).ready(function () {
     for (let i in compositions) {
 	let a = `<li><a id="comp${i}" title="Click to load ${compositions[i]}"
-	href="#comp${i}" onclick="loadabc(${i});return false;">${compositions[i]}</a></li>`;
+	href="#comp${i}" onclick="loadsrg(${i});return false;">${compositions[i]}</a></li>`;
 	$("#list").append(a);
     }
 });
+
+function loadsrg(i) {
+    fetch(compositions[i] + ".srg")
+	.then(response => response.text())
+	.then(data => create_abc(data.split("\n")));
+};
+
+function note_length(word) {
+    word = word.replaceAll(',', '');
+    return word.length;
+}
+
+function convert_notation (line) {
+    let notes = line.replaceAll("- - - -", "z4");
+    notes = notes.replaceAll(" - - -", "4");
+    notes = notes.replaceAll(" - -", "3");
+    notes = notes.replaceAll(" -", "2");
+    notes = notes.replaceAll("|3", "|z2");
+    notes = notes.replaceAll("|2", "|z");
+    notes = notes.replaceAll(" =", "12");
+    
+    let words = [];
+    for (let word of notes.split(" ")) {
+	if (note_length(word) == 2 && word.match(/^[,srgmpdn]+$/ig)) {
+	    let chars = word.split('');
+	    word = chars.join('/') + '/';
+	}
+	words.push (word);
+    }
+    notes = words.join(' ');
+    let abc="";
+    for (let i = 0; i < notes.length; i++)
+	abc += convert_note(notes[i] );
+    
+    abc = abc.replaceAll("|| ", '| "^X"');
+    return abc;
+}
+
+
+function create_abc (lines) {
+    let abc="";
+    for (let i in lines) {
+	let fields = lines[i].split(':');
+	if (fields[0] != "srg") abc += lines[i];
+	else abc += convert_notation(fields[1]);
+	abc += '\n';
+    }
+    showabc(abc);
+}
 
 function loadabc(i) {
     fetch(compositions[i])
