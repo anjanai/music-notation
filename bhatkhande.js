fd0f29454e502202558125ec14d58a8d94c1778a
anjanai <anjanai.yahoo@gmail.com>
2024-02-15 22:48:26 -0800

    Add jog and update bhatkhande, chandrakauns

--- bhatkhande.js
+++ bhatkhande.js
@@ -1,3 +1,28 @@
+const x='x';
+const taali_khali = {
+    'teentaal' : [x, 2, 0, 3],
+    'ektaal' : [x, 0, 2, 0, 3, 4],
+    'jhaptaal' : [x, 2, 0, 3],
+    'rupak': [0, 1, 2],
+    'keherva': [x, 0],
+    'dadra': [x, 0],
+    'ada-chautaal': [x, 2, 0, 3, 0, 4, 0],
+};
+
+const matras = {
+    'teentaal' : [4, 4, 4, 4],
+    'ektaal' : [2, 2, 2, 2, 2, 2],
+    'jhaptaal' : [2, 3, 2, 3],
+    'rupak': [3, 2, 2],
+    'keherva': [4, 4],
+    'dadra': [3, 3],
+    'ada-chautaal': [2, 2, 2, 2, 2, 2, 2],
+	
+};
+
+    
+
+
 function changeScript() {
     if ($('#button_changeScript').text() === "Change to Hindi") {
 	$('table').removeClass("bhatkhande_english").addClass("bhatkhande_hindi");
@@ -8,71 +33,138 @@ function changeScript() {
     }
 }
 
-/*
-Lower octave:
-g m d N
-4 3 2 1
-*/
-function to_chandrakauns(str) {
+function to_bhatkhande(raag, str) {
+    switch (raag) {
+    case "jog":
+	/*
+	  Input:
+	  Lower octave:
+	  m p n
+	  ml pl nl
+	  Middle octave: s _g g m p n
+	  Higher octave: S _G G M P
+	  
+	  Output:
+	  Lower ocatve: pl Nl
+	  Middle octave: s G g m p n
+	  Higher octave: Su Gu gu mu pu
+	*/
+		
+	str = str.replace(/1/g, 'Nl');
+	str = str.replace(/2/g, 'pl');
+	
+	str = str.replace(/G/g, 'gu');
+	str = str.replace(/_G/g, 'Gu');
+	str = str.replace(/M/g, 'mu');
+	
+	str = str.replace(/_g/g, 'G');
+	str = str.replace(/n/g, 'N');
+	str = str.replace(/S/g, 'su');
+	break;
+	
+    case "chandrakauns" :
+	/*
+	  Input:
+	  Lower octave: ml dl nl
+	  Middle octave: s g m d n
+	  Higher octave: S G M
+	  
+	  Output:
+	  Lower ocatve: Dl nl
+	  Middle octave: s G m D n
+	  Higher octave: Su Gu mu
+	*/
+	
+	str = str.replace(/G/g, 'Gu');
+	str = str.replace(/M/g, 'mu');
+	
+	str = str.replace(/g/g, 'G');
+	str = str.replace(/d/g, 'D');
+	str = str.replace(/S/g, 'su');
+	break;
+    }
 
-    str = str.replace(/1/g, 'nl');
-    str = str.replace(/2/g, 'Dl');
-    
-    str = str.replace(/G/g, 'Gu');
-    str = str.replace(/M/g, 'mu');
-    
-    str = str.replace(/g/g, 'G');
-    str = str.replace(/d/g, 'D');
-    str = str.replace(/S/g, 'su');
+    str = str.replace(/\(/g, "<sup>");
+    str = str.replace(/\)/g, "</sup>");
     return str;
 }
 
 
 $(document).ready(function () {
     $('span:hidden').each(function() {
-	convert_notation($(this));
-	
+	convert_notation($(this));	
     });
 });
 
-function convert_notation(obj) {
-    let notation = obj.text().trim().replace(/\|\|/g, '|');
-    let bhat = '<table class="composition bhatkhande_english"><tbody>';
-    let line_num = 0;
-    let lines = notation.split("\n");
+function format(line, taal, raag, start_at) {
+    str = "";
+    line = line.replace(/\|/g, "").trim();
+    if (line === "") return "";
 
-    for (let line of lines) {
-	line = line.trim();
-	if (line === "") continue;
-	line = line.replace(/^\|+/, "");
-	
-	bhat += "\n<tr>"
-	let divs = line.split("|");
-	if (line_num == 0) {
-	    if (divs.length > 3)  bhat += "<td>0</td><td>4</td><td>x</td><td>3</td><td></tr><tr>";
-	    else  bhat += "<td></td><td>x<td>3<td></tr><tr>";
-	}
-	line_num++;
-	if (obj.attr('other') === "taans")
-	    bhat += "<td width=10%><pre>" + line_num + "</pre></td>";
+    line = to_bhatkhande(raag, line);
+    
+    let beats = line.split(/\s+/);
+    let divs = matras[taal];
 
-	
-	for (let div of divs) {
-	    bhat += "\n<td>";
-		
-	    let beats = div.trim().split(/\s+/g);
-	    for (let beat of beats) {
-		if (beat.length == 2)
-		    beat = "@" + beat;
-		    
-		bhat += to_chandrakauns(beat) + " ";
+    let i=0;
+    for (let div of divs) {
+	let vibhag = beats.slice(i, i+div);
+	str += "<td>";
+	for (let v of vibhag) {
+	    if (v.length > 1 &&  /[srgmpdn-]/i.test(v[0])) {
+		let removed_ul = v.replace(/[u|l]/g, '');
+		if (removed_ul.length == 2)
+		    v = "@"+v;	
 	    }
-	    bhat += "</td>";
+	    str += v + " ";
+
 	}
-	bhat += "\n</tr>";
+	str += "</td>";
+	i += div;
     }
-    bhat += "</tbody></table>";
-    console.log ("#" + obj.attr('other'));
-    $("#" + obj.attr('other')).html(bhat);
+
+    
+    return str;
+}
+
+const rotateArray = (arr, k) => arr.slice(k).concat(arr.slice(0, k));
+
+
+
+function format_taal(taal, start_at) {
+    let beats=0;
+    
+    for (i=0; beats < start_at; i++) {
+	beats += matras[taal][i];
+    }
+        
+    let tk = rotateArray(taali_khali[taal], i-1);
+
+    str = "\n<tr>"
+    for (let i of tk) 
+	str += "<td>"+i+"</td>";
+    
+    str += "</tr>";
+    return str;
+}
+
+function convert_notation(obj) {
+    let raag = $('#raag').val();
+    let taal = obj.attr('taal');
+
+    let str = '<table class="composition bhatkhande_english"><tbody>';
+    let lines = obj.text().trim().split("\n");
+
+    // taal
+    str += format_taal(taal, obj.attr('start_at'))
+    let linenum = 1
+    for (let line of lines) {
+	str += "<tr>" + format(line, taal, raag);
+	if (obj.attr('other') === "taans")
+	    str += "<td><pre>Taan " + linenum++;
+	str += "</tr>";	
+    }
+    str += "</tbody></table>";
+    $("#" + obj.attr('other')).html(str);
 }
     
