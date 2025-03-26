18107a897e9d79dbe0f8ade4a621cf3c8cb1e6e2
anjanai <anjanai.yahoo@gmail.com>
2025-03-26 14:03:15 -0700

    Add conjunct characters

--- bhatkhande_font_convert.js
+++ bhatkhande_font_convert.js
@@ -5,12 +5,13 @@ function tr(input, search, replace) {
 
     input = input.replaceAll('ü','');
     input = input.replaceAll('û','');
-    
+
+    input = input.replaceAll('ÌÄlÉ', "िऩ");
+    input = input.replaceAll('xÉÇÉ', 'xÉÉÇ');
+        
     searchArray.forEach((char, index) => {
         translationMap[char] = replaceArray[index] || char;
     });
-
-    console.log (translationMap);
     
     return input
         .split("")
@@ -18,23 +19,27 @@ function tr(input, search, replace) {
         .join("");
 }
 
-var english = "A C D E F L M N O P Q S T U W Z a b c e f h i j k l m o p q r s u x z § ¨ ± ´ Â Ã Æ Ç É Ë Ì Í Î Ï Ð Ñ Ò Ô Õ × å æ ë Å þ Ä ð";
+var english = "A C D E F G L M N O P Q R S T U W Z a b c e f h i j k l m o p q r s u w x z § ¨ ± ´ Â Ã Æ Ç É Ë Ì Í Î Ï Ð Ñ Ò Ô Õ × å æ ë Å þ Ä ð ï ì ç ¹ ² ³ © µ ¸ ¥ ® ª £ Y ¼";
+var hindi =   "अ इ ई उ ऊ ऋ ए क छ ट ठ ड ढ द फ र ह ख् ग् घ् च् ज् झ् ण् त् थ् ध् न् प् ब् भ् म् य् ल् व् ष् स् श् त्र् त्त् द्य् श्र् रु रू ँ ं ा ि ि ि ि ी ी ु ु ू ू ृ े ै ्र ऽ ऺ ़ ँ र् ्र ् ष्ट द्व न्न् द्म श्व ष्ठ ज्ञ् द्ध द्ग क्त क् ह्म";
 
 
-// "ë ì ï ð û ü þ";
-var hindi = "अ इ ई उ ऊ ए क छ ट ठ ड द फ र ह ख् ग् घ् च् ज् झ् ण् त् थ् ध् न् प् ब् भ् म् य् ल् व् स् श् त्र् त्त् द्य् श्र् रु रू ँ ं ा ि ि ि ि ी ी ु ु ू ू ृ े ै ्र ऽ ऺ ़ ँ";
 
 function convertFont() {
     $.get('bhatkhande_lyrics.txt', function(str) {
-	//str = "ÌlÉkÉÅmÉ";
+	//str = "mÉÉMü  2ÉeSå ";
 	let conv = tr(str, english, hindi);
 	conv2 = conv.replaceAll('्ा','');
 	conv2 = conv2.replaceAll('ाै', 'ौ');
 	conv2 = conv2.replaceAll('ाे', 'ो');
 	conv2 = conv2.replaceAll("एे", 'ऐ');
 	conv2 = conv2.replaceAll("अा", 'आ');
-
-	conv2 = conv2.replaceAll(/ि([\u0900-\u097F])/ug,"$1ि");
+	conv2 = conv2.replaceAll("अो", 'ओ');
+	conv2 = conv2.replaceAll("अौ", 'औ');
+	conv2 = conv2.replaceAll("स्ंाा", "सां");
+	conv2 = conv2.replaceAll(/ि([\u0915-\u0939])/ug,"$1ि");  
+	conv2 = conv2.replaceAll(/़([\u0915-\u0939])/ug,"$1़");
+	conv2 = conv2.replaceAll(/([\u0915-\u0939])र्/ug,"र्$1");
+	
 	//console.log (conv, conv2);
 	saveTextAsFile(conv2, "Bhatkhande_Lyrics.txt");
 	$("#msg").text("Converted! Please check your Downloads folder.");
